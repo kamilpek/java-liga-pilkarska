00a7a0ab645dc6d6e7bebd9ebbf372dcd9b1f41e
Kamil Pek <kpek@sigma.ug.edu.pl>
2016-01-04 19:41:53 +0100

    backup 2

--- src/ligapilkarska/controller.java
+++ src/ligapilkarska/controller.java
@@ -9,6 +9,9 @@ public class controller {
     private view liga_view;
     private java.util.ArrayList<String> lista_1kolejka = new ArrayList<>();
     private java.util.ArrayList<String> lista_2kolejka = new ArrayList<>();
+    private java.util.ArrayList<String> lista_3kolejka = new ArrayList<>();
+    private java.util.ArrayList<String> lista_4kolejka = new ArrayList<>();
+    String polecenie = null;
 
     controller(model model, view view){
         liga_model = model;
@@ -16,26 +19,46 @@ public class controller {
 
         lista_1kolejka = model.select_1kolejka();
         lista_2kolejka = model.select_2kolejka();
+        lista_3kolejka = model.select_3kolejka();
+        lista_4kolejka = model.select_4kolejka();
         view.getprzycisk_1kolejka().addActionListener(new NasluchiwaczPrzyciskow());
         view.getprzycisk_2kolejka().addActionListener(new NasluchiwaczPrzyciskow());
+        view.getlista_selectkolejki().addActionListener(new NasluchiwaczPrzyciskow());
+        
     }
 
     private class NasluchiwaczPrzyciskow implements ActionListener {
 
         public void actionPerformed(ActionEvent e) {
+            polecenie = view.getlista_selectkolejki().getSelectedItem().toString();
+            
             String command = e.getActionCommand();
-            if (command.equals("1-kolejka")) {
+            if ((command.equals("1-kolejka"))||polecenie.equals("1. kolejka.")) {
                 String listawyniku = "";
                 for (String s : lista_1kolejka) {
                     listawyniku += s;
                 }
                 view.select.setText(listawyniku);
             }
-            else if (command.equals("2-kolejka")) {
+            else if ((command.equals("2-kolejka")||(polecenie.equals("2. kolejka.")))) {
                 String listawyniku = "";
                 for (String s : lista_2kolejka) {
                     listawyniku += s;
                 }                
+                view.select.setText(listawyniku);
+            }
+            else if (polecenie.equals("3. kolejka.")) {
+                String listawyniku = "";
+                for (String s : lista_3kolejka) {
+                    listawyniku += s;
+                }                
+                view.select.setText(listawyniku);                
+            }
+            else if (polecenie.equals("4. kolejka.")) {
+                String listawyniku = "";
+                for (String s : lista_4kolejka) {
+                    listawyniku += s;
+                }
                 view.select.setText(listawyniku);
             }
         }
--- src/ligapilkarska/view.java
+++ src/ligapilkarska/view.java
@@ -22,6 +22,14 @@ public class view extends JFrame {
     public static JButton getprzycisk_2kolejka() {
         return przycisk_2kolejka;
     }
+    
+    public static JButton getprzycisk_test() {
+        return przycisk_test;
+    }
+    
+    public static JComboBox getlista_selectkolejki() {
+        return lista_selectkolejki;
+    }
 
     private int szerokosc = 800;
     private int wysokosc = 600;
@@ -32,7 +40,9 @@ public class view extends JFrame {
     private JLabel gornynapis;
     public static JButton przycisk_1kolejka = new JButton("1. Kolejka");
     public static JButton przycisk_2kolejka = new JButton("2. Kolejka");
+    public static JButton przycisk_test = new JButton("Przycisk testowy");
     public static JTextArea select = new JTextArea(8, 60);
+    public static JComboBox lista_selectkolejki = new JComboBox();
 
     public void TextArea_Select() {
         okno = new JFrame(tytul);
@@ -44,13 +54,23 @@ public class view extends JFrame {
         JScrollPane ScrollPane = new JScrollPane(select);
         przycisk_1kolejka.setActionCommand("1-kolejka");
         przycisk_2kolejka.setActionCommand("2-kolejka");
+        przycisk_test.setActionCommand("test");
+        
+        lista_selectkolejki.setBounds(80, 40, 170, 20);
+        lista_selectkolejki.addItem("1. kolejka.");
+        lista_selectkolejki.addItem("2. kolejka.");
+        lista_selectkolejki.addItem("3. kolejka.");
+        lista_selectkolejki.addItem("4. kolejka.");
+        lista_selectkolejki.addItem("5. kolejka.");
 
         panel = new JPanel();
         panel.setLayout(new FlowLayout());
         okno.add(panel);
         okno.add(gornynapis);
+        panel.add(lista_selectkolejki);
         panel.add(przycisk_1kolejka);
         panel.add(przycisk_2kolejka);
+        panel.add(przycisk_test);        
         panel.add(select);
         panel.add(ScrollPane);
         okno.setVisible(true);
